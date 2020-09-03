10ec1372dfd6bd1b56b4c7269153fbf505e1488a
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-09-03 08:09:27 +0300

    Adds test for SoloDetailView and stubs view class

--- jmad/solos/views.py
+++ jmad/solos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
 from .models import Solo
 
@@ -24,3 +25,7 @@ def index(request):
         context['solos'] = solos_queryset
         
     return render(request, 'solos/index.html', context)
+
+
+class SoloDetailView(DetailView):
+    pass
