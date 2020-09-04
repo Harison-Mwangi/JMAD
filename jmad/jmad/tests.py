748e8150c7d9e7c2c8412816a9787e4b3abe4b34
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-09-04 10:48:00 +0300

    Modified CI workflow to run unit tests only

--- jmad/jmad/tests.py
+++ jmad/jmad/tests.py
@@ -1,21 +1,15 @@
-from django.test import LiveServerTestCase
+from django.test import LiveServerTestCase, tag
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from decouple import config
 
 from solos.models import Solo
 
+@tag('functional')
 class StudentTestCase(LiveServerTestCase):
 
     def setUp(self):
-        if config('SELENIUM_SERVER', default='') != '':
-            self.browser = webdriver.Remote(
-                command_executor=config('SELENIUM_SERVER'),
-                desired_capabilities=DesiredCapabilities.FIREFOX
-            )
-        else:
-            self.browser = webdriver.Firefox()
-
+        self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(2)
 
         self.solo1 = Solo.objects.create(
