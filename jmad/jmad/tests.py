8327c9acb4d8a7b5a1ff062a05d1597d83aae298
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-09-04 09:56:06 +0300

    Attempt to run selenium server

--- jmad/jmad/tests.py
+++ jmad/jmad/tests.py
@@ -1,12 +1,21 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from decouple import config
 
 from solos.models import Solo
 
 class StudentTestCase(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        if config('SELENIUM_SERVER', default='') != '':
+            self.browser = webdriver.Remote(
+                command_executor=config('SELENIUM_SERVER'),
+                desired_capabilities=DesiredCapabilities.FIREFOX
+            )
+        else:
+            self.browser = webdriver.Firefox()
+
         self.browser.implicitly_wait(2)
 
         self.solo1 = Solo.objects.create(
