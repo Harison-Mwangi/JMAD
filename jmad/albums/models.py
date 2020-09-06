6a75f3a40c92e62e6c58062bfe672261793c9fcd
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-09-06 13:54:32 +0300

    Adds string representation for Track

--- jmad/albums/models.py
+++ jmad/albums/models.py
@@ -4,6 +4,10 @@ class Album(models.Model):
     name = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
     slug = models.SlugField()
+    
+    def __str__(self):
+        return self.name
+
 
 class Track(models.Model):
     name = models.CharField(max_length=100)
